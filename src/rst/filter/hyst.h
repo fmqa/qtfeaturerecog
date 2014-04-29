e794a5cd7f49455234f2cb46b0fcf70d8989b350
Joseph F. Mq. Kochab <smfadi+github@gmail.com>
2014-04-29 13:27:06 +0200

    Add Qt skeleton

--- src/rst/filter/hyst.h
+++ src/rst/filter/hyst.h
@@ -7,16 +7,15 @@
 namespace rst {
     template <typename F, typename G, typename T, typename U>
     void mark_edge (F f, G g, T m, T n, T i, T j, U tmin) {
-        using V = typename std::remove_reference<decltype(g(i,j))>::type;
         T y0 = i ? i - 1 : i;
         T x0 = j ? j - 1 : j;
         T y1 = (i == m - 1) ? i : i + 1;
         T x1 = (j == n - 1) ? j : j + 1;
-        g(i, j) = std::numeric_limits<V>::max();
+        g.set(i, j, std::numeric_limits<decltype(g.get(i,j))>::max());
         for (; y0 <= y1; ++y0) {
             for (; x0 <= x1; ++x0) {
                 if ((y0 != y1 || x0 != x1) 
-                    && g(y0, x0) == 0 
+                    && g.get(y0, x0) == 0 
                     && f(y0, x0) >= tmin)
                 {
                     mark_edge(f, g, m, n, y0, x0, tmin);
@@ -30,7 +29,7 @@ namespace rst {
     void hyst (F f, G g, T m, T n, U tmin, U tmax) {
         for (T i = 0; i < m; ++i) {
             for (T j = 0; j < n; ++j) {
-                if (g(i, j) == 0 && f(i, j) >= tmax) {
+                if (g.get(i, j) == 0 && f(i, j) >= tmax) {
                     mark_edge(f, g, m, n, i, j, tmin);
                 }
             }
---
+++ src/simple.cc
@@ -0,0 +1,43 @@
+#include <QApplication>
+#include <QImage>
+#include <QLabel>
+#include "rst/wrap.h"
+#include "rst/util.h"
+#include "rst/filter/gaussian.h"
+#include "rst/filter/canny.h"
+
+static rst::gaussian_filter<3> stdgaussian(1.0);
+
+struct ImageProxy {
+    ImageProxy(QImage *img) : image(img) {}
+    
+    unsigned char get(int y, int x) const { return qGray(image->pixel(x, y)); }
+    unsigned char operator() (int y, int x) const { return get(y, x); }
+    void set(int y, int x, char v) const { image->setPixel(x, y, qRgb(v,v,v)); }
+    
+private:
+    QImage *image;
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QImage in;
+    in.load(argv[1]);
+    
+    QImage out(in.size(), QImage::Format_RGB888);
+    
+    auto wrapper = rst::make_ext_wrapper(ImageProxy(&in), in.height(), in.width());
+    rst::canny([&wrapper](int y, int x){return stdgaussian.apply(wrapper,y,x);},
+               ImageProxy(&out),
+               rst::fasthypot<std::pair<double,double>>,
+               in.height(), in.width(), 1, 30);
+    
+    QLabel myLabel;
+    myLabel.setPixmap(QPixmap::fromImage(out));
+
+    myLabel.show();
+
+    return a.exec();
+}
