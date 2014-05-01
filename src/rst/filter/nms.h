7c9cb43993dc4614e250a74e3d2d8a8e91be08ba
Joseph F. Mq. Kochab <smfadi+github@gmail.com>
2014-05-01 23:10:12 +0200

    Minor renaming

--- src/rst/filter/nms.h
+++ src/rst/filter/nms.h
@@ -8,30 +8,30 @@
 namespace rst {
     namespace __rst_filter_detail {
         template <typename T, typename U>
-        auto proxy(T p, U binaryfn) -> decltype(binaryfn(p.first, p.second)) {
+        auto eval(T p, U binaryfn) -> decltype(binaryfn(p.first, p.second)) {
             return binaryfn(p.first, p.second);
         }
     }
     
     template <typename F, typename H, typename T>
     auto nms (F f, H h, T y, T x) 
-        -> decltype(__rst_filter_detail::proxy(sobel(f, y, x), h))
+        -> decltype(__rst_filter_detail::eval(sobel(f, y, x), h))
     {
         auto gyx = sobel(f, y, x);
         float d = 8 * fmod(atan2(gyx.first, gyx.second) + M_PI, M_PI) / M_PI;
-        auto fyx = __rst_filter_detail::proxy(sobel(f, y, x), h);
+        auto fyx = __rst_filter_detail::eval(sobel(f, y, x), h);
         return (((d <= 1 || d > 7) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y,x-1), h)
-                && fyx > __rst_filter_detail::proxy(sobel(f,y,x+1), h))
+                && fyx > __rst_filter_detail::eval(sobel(f,y,x-1), h)
+                && fyx > __rst_filter_detail::eval(sobel(f,y,x+1), h))
             || ((d > 1 && d <= 3) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y-1,x+1), h) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y+1,x-1), h))
+                && fyx > __rst_filter_detail::eval(sobel(f,y-1,x+1), h) 
+                && fyx > __rst_filter_detail::eval(sobel(f,y+1,x-1), h))
             || ((d > 3 && d <= 5) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y-1,x), h) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y+1,x), h))
+                && fyx > __rst_filter_detail::eval(sobel(f,y-1,x), h) 
+                && fyx > __rst_filter_detail::eval(sobel(f,y+1,x), h))
             || ((d > 5 && d <= 7) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y-1,x-1), h) 
-                && fyx > __rst_filter_detail::proxy(sobel(f,y+1,x+1), h)))
+                && fyx > __rst_filter_detail::eval(sobel(f,y-1,x-1), h) 
+                && fyx > __rst_filter_detail::eval(sobel(f,y+1,x+1), h)))
             ? fyx
             : 0;  
     }
