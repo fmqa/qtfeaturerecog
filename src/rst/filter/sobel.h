24b82d12b17b8838f70e78a675b6c640e9f4830a
Joseph F. Mq. Kochab <smfadi+github@gmail.com>
2014-05-01 23:07:16 +0200

    Simulate C++1y's decltype(auto) here again. Sigh.

--- src/rst/filter/sobel.h
+++ src/rst/filter/sobel.h
@@ -8,7 +8,9 @@ namespace rst {
     extern const int sobel_x_kernel[3][3], sobel_y_kernel[3][3];
     
     template <typename F, typename T>
-    auto sobel (F f, T y, T x) -> std::pair<decltype(f(x,y)),decltype(f(x,y))> 
+    auto sobel (F f, T y, T x) 
+        -> decltype(std::make_pair(conv(sobel_y_kernel, f, y, x),
+                                   conv(sobel_x_kernel, f, y, x)))
     {
         return std::make_pair(conv(sobel_y_kernel, f, y, x),
                               conv(sobel_x_kernel, f, y, x));
